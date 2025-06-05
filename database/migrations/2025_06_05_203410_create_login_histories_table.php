8e73e526f930fb772eb8f40ca18d482c0fd738a0
ShehrrYarr <shehrryarr@gmailcom>
2025-06-05 20:35:41 +0500

    login history template added

---
+++ database/migrations/2025_06_05_203410_create_login_histories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoginHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('login_histories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->time('start_time'); // e.g., 09:00
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('login_histories');
+    }
+}
